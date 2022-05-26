766a6cfa9160bdf8090e32dce45cc8fb55fb46ec
Valeriy Sheplyakov <valeriy.sheplyakov@gmail.com>
2022-05-26 17:12:01 +0300

    edited carousel company page

--- js/partners-carousel.js
+++ js/partners-carousel.js
@@ -1,24 +1,42 @@
-(function() {
-    const slides = [
-       '<div class="slides"><img src="img/partners-img/aecom.svg" alt="AECOM"></div>',
-       '<div class="slides"><img scr="img/partners-img/fedex.svg" alt="Fedex"></div>',
-       '<div class="slides"><img scr="img/partners-img/giffels.svg" alt="Giffels"></div>',
-       '<div class="slides"><img scr="img/partners-img/interior.svg" alt="Interior"></div>',
-       '<div class="slides"><img scr="img/partners-img/lowes.svg" alt="Lowes"></div>' 
-    ];
+(function () {
+  const slides = [
+    '<div class="partners-slide"><img src="img/partners-img/aecom.svg" alt="AECOM"></div>',
+    '<div class="partners-slide"><img src="img/partners-img/fedex.svg" alt="Fedex"></div>',
+    '<div class="partners-slide"><img src="img/partners-img/giffels.svg" alt="Giffels"></div>',
+    '<div class="partners-slide"><img src="img/partners-img/interior.svg" alt="Interior"></div>',
+    '<div class="partners-slide"><img src="img/partners-img/lowes.svg" alt="Lowes"></div>',
+  ];
 
-    let currentSlide = 0;
-    
-    function renderCarousel() {
-        const carouselContainer = document.querySelector(.'carousel-partners');
-        carouselContainer.innerHTML = slides[currentSlide];
+  let currentSlide = 0;
+
+  function renderCarousel() {
+    const slideContainer = document.querySelector('.carousel-partners');
+    slideContainer.innerHTML = slides[currentSlide];
+    if (window.innerWidth >= 600) {
+      const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+      slideContainer.innerHTML += slides[secondSlide];
+      if (window.innerWidth >= 900) {
+        const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
+        slideContainer.innerHTML += slides[thirdSlide];
+        if (window.innerWidth >= 1200) {
+          const fourthSlide = thirdSlide + 1 >= slides.length ? 0 : thirdSlide + 1;
+          slideContainer.innerHTML += slides[fourthSlide];
+          if (window.innerWidth >= 1600) {
+            const fifthSlide = fourthSlide + 1 >= slides.length ? 0 : fourthSlide + 1;
+            slideContainer.innerHTML += slides[fifthSlide];
+          }
+        }
+      }
     }
+  }
 
-    function nextSlide() {
-        currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-        renderCarousel();
-    }
+  function nextSlide() {
+    currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
+    renderCarousel();
+  }
 
-    setInterval(nextSlide, 3000);
+  setInterval(nextSlide, 5000);
+  renderCarousel(0);
 
+  window.addEventListener('resize', renderCarousel);
 })();
