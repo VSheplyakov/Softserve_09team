870d30794054683f131a2f702e86322240020bc2
Maxim <djclive@gmail.com>
2022-05-27 15:34:53 +0000

    edit js service

--- js/service-collapse.js
+++ js/service-collapse.js
@@ -1,31 +1,24 @@
-
-const forClickButtons = document.getElementsByClassName('service_btn_collapce');
+const forClickButtons = document.getElementsByClassName("service_btn_collapce");
 let collapseOpen = 0;
 
-for(let button of forClickButtons) {
-    button.onclick = openCollapseItem;
+for (let button of forClickButtons) {
+  button.onclick = openCollapseItem;
 }
 
+function openCollapseItem() {
+  const elem = this.closest(".service_img_block");
 
-
-function openCollapseItem(){
-
-
-const elem = this.closest('.service_img_block');
-
-
-    if(collapseOpen == 0){
-        elem.querySelector('.service_hide_info').style.display = 'block'; 
-        this.innerHTML = '-';
-        collapseOpen = 1;
-        return;
-    }
-    if(collapseOpen == 1){
-        elem.querySelector('.service_hide_info').style.display = 'none'; 
-        this.innerHTML = '+';
-        collapseOpen = 0;
-        return;
-    }
-
+  if (collapseOpen == 0) {
+    elem.querySelector(".service_hide_info").style.display = "block";
+    this.innerHTML = "-";
+    collapseOpen = 1;
+    return;
+  }
+  if (collapseOpen == 1) {
+    elem.querySelector(".service_hide_info").style.display = "none";
+    this.innerHTML = "+";
+    collapseOpen = 0;
+    return;
+  }
 }
 
