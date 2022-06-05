8f7c0945a854d76f830ebc4b63c1aea906a547d1
Maxim <djclive@gmail.com>
2022-06-05 19:30:04 +0000

    Service fix some css code

--- js/service-collapse.js
+++ js/service-collapse.js
@@ -1,5 +1,5 @@
 const forClickButtons = document.getElementsByClassName("service_btn_collapce");
-let collapseOpen = 0;
+
 
 for (let button of forClickButtons) {
   button.onclick = openCollapseItem;
@@ -8,16 +8,13 @@ for (let button of forClickButtons) {
 function openCollapseItem() {
   const elem = this.closest(".service_img_block");
 
-  if (collapseOpen == 0) {
+  if (elem.querySelector(".service_hide_info").style.display == "none" || elem.querySelector(".service_hide_info").style.display == "") {
     elem.querySelector(".service_hide_info").style.display = "block";
     this.innerHTML = "-";
-    collapseOpen = 1;
     return;
-  }
-  if (collapseOpen == 1) {
+  }else{
     elem.querySelector(".service_hide_info").style.display = "none";
     this.innerHTML = "+";
-    collapseOpen = 0;
     return;
   }
 }
