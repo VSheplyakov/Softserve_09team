1ed28d1f9f1a45275de35c17e7d732f2858ba89f
grinata101@gmail.com <grinata101@gmail.com>
2022-06-07 12:27:16 +0300

    add new position to baner

--- js/head.js
+++ js/head.js
@@ -1,9 +1,9 @@
 function closeBurger(){
-let toggleBlock = document.getElementById("heder-menu-toggle");
-toggleBlock.checked=false;
-}
-let allNav = document.querySelectorAll(".nav-link-page-toggl a");
-
-for(let i=0; i<allNav.length; i++){
-    allNav[i].addEventListener("click", closeBurger)
-}
+    let toggleBlock = document.getElementById("heder-menu-toggle");
+    toggleBlock.checked=false;
+    }
+    let allNav = document.querySelectorAll(".nav-link-page-toggl a");
+    
+    for(let i=0; i<allNav.length; i++){
+        allNav[i].addEventListener("click", closeBurger)
+    };
